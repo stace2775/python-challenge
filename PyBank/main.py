308586c5e93725b4847d4d399b2cdf81c5c44ddc
Stacey <stacey.ellen.smith@gmail.com>
2019-06-12 13:35:05 -0500

    homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,18 @@
-{
-	"folders": [
-		{
-			"path": "C:\\Users\\stacey.smith\\source\\python"
-		}
-	],
-	"settings": {}
-}
+## Description:
+#
+#
+# Modification History:
+#	DD-MM-YYY 	Author			Description
+#	12-06-2019	Stacey Smith	Initial Creation
+#
 
-##
+
+import os
+import csv
+
+file = os.path.join("..", "Resources", "budget_data.csv")
+
+with open(file, newline='') as csvfile:
+	csvreader = csv.reader(csvfile, delimiter=",")
+	csvheader = next(csvreader)
+	print(f'CSV Header {csvheader}')
